f62510bb7c7b47dac503106a88de5ea58711eae8
Alex <alexbrence@protonmail.com>
2023-04-28 22:23:06 +0200

    fixed some security issues

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
@@ -11,6 +12,16 @@ from django.views.generic import (
 )
 
 from .models import Task
+
+
+class PreventUnauthorizedMixin(object):
+    def get(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=kwargs['pk'])
+
+        if task.user != request.user:
+            raise Http404
+
+        return super().get(request, *args, **kwargs)
 
 
 class CustomLoginView(LoginView):
@@ -46,9 +57,16 @@ class TaskList(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        
+        # User tasks
         user_tasks = context['tasks'].filter(user=self.request.user)
         context['tasks'] = user_tasks
         context['count'] = user_tasks.filter(is_completed=False).count()
+
+        # Searching
+        if search_input := self.request.GET.get('search', ''):
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+        context['search'] = search_input
         
         return context
 
@@ -56,6 +74,10 @@ class TaskList(LoginRequiredMixin, ListView):
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
+
+    def get_queryset(self):
+        # Prevent user to see items from other users
+        return Task.objects.filter(user=self.request.user)
 
 
 class TaskCreate(LoginRequiredMixin, CreateView):
@@ -68,15 +90,14 @@ class TaskCreate(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class TaskUpdate(LoginRequiredMixin, UpdateView):
+class TaskUpdate(PreventUnauthorizedMixin, LoginRequiredMixin, UpdateView):
     model = Task
     fields = ['title', 'description', 'is_completed']
     success_url = reverse_lazy('task-list')
 
 
-class TaskDelete(LoginRequiredMixin, DeleteView):
+class TaskDelete(PreventUnauthorizedMixin, LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('task-list')
-
 
